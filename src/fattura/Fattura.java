5653bce8887dba74911453b4d90464745a15628a
4DS-paoloceschini <paolo.ceschini@buonarroti.tn.it>
2024-01-16 08:27:23 +0100

    16/01/2024: classi ora e data.2

--- src/fattura/Fattura.java
+++ src/fattura/Fattura.java
@@ -1,48 +1,19 @@
 package fattura;
 
-public class Fattura {
-    public class DocumentoFiscale {
+public class Fattura extends DocumentoFiscale {
         protected Data data;
         protected Ora ora;
         private Azienda azienda;
         private Prodotto[] carrello;
         private int[] numProdotti;
 
-        DocumentoFiscale (Data data, Ora ora, Azienda azienda, Prodotto[] carrello, int[] numProdotti){
-            this.data = data;
-            this.ora = ora;
-            this. azienda = azienda;
-            this.carrello = carrello;
-            this.numProdotti = numProdotti;
-        }
-
-        public Data getData() {
-            return data;
-        }
-
-        public Ora getOra() {
-            return ora;
-        }
-
-        public Azienda getAzienda() {
-            return azienda;
-        }
-
-        public Prodotto[] getCarrello() {
-            return carrello;
-        }
-
-        public int[] getNumProdotti() {
-            return numProdotti;
+        Fattura(Data data, Ora ora, Azienda azienda, Prodotto[] carrello, int[] numProdotti) {
+            super(data, ora, azienda, carrello, numProdotti);
         }
 
         @Override
         public String toString() {
-            String strRet = "";
-            for (int i = 0; i< carrello.length; i++){
-                strRet += carrello[i] + "\n" + numProdotti + "\n";
-            }
-            return strRet;
+
         }
     }
 

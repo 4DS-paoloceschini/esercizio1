9e6457e985c8d3e0030761bf9354c668ff2422c4
pablito <145672204+4DS-paoloceschini@users.noreply.github.com>
2024-04-09 09:42:23 +0200

    Create HashTable1.java

---
+++ src/HashTable1.java
@@ -0,0 +1,41 @@
+package ceschini;
+
+public class HashTable1{
+
+    private PersonaHT[] ELEMENTS;
+
+    public HashTable1 (){
+        ELEMENTS = new PersonaHT[11];
+    }
+
+    public PersonaHT[] getELEMENTS() {
+        return ELEMENTS;
+    }
+
+    public void addElement(PersonaHT element) {
+        ELEMENTS[findPosition(element)] = element;
+    }
+
+    public Integer findElement(PersonaHT element) {
+        if (ELEMENTS[findPosition(element)].getNome().equals(element.getNome())){
+            return findPosition(element);
+        }
+        else {
+            return -1;
+        }
+    }
+
+    //@Override
+    private Integer findPosition(PersonaHT element) {
+        return element.getAscii()%11; //modulo di questo
+    }
+
+    public String toString(){
+        String str = "[ ";
+        for (int i = 0; i<11; i++){
+            str += ELEMENTS[i].getNome() + " " + ELEMENTS[i].getDataDiNascita() + ", ";
+        }
+        str += " ]";
+        return str;
+    }
+}
